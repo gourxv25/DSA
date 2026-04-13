2aa7186c9ad5c606301ac882f76fb0296addf427
Gourav Chouhan <gouravchouhan2512@gmail.com>
2026-04-13 12:06:22 +0530

    Time: 2 ms (81.94%) | Memory: 46.5 MB (84.47%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,54 @@
+class Solution {
+    List<List<String>> result = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for(int i = 0; i < n; i++ )
+            Arrays.fill(board[i], '.');
+        
+        generate(board, 0);
+        
+        return result;
+    }
+
+    public void generate(char[][] board, int row){
+        if(row == board.length){
+            List<String> list = new ArrayList<>();
+            for(int i = 0; i < board.length; i++){
+                list.add(new String(board[i]));
+            }
+            result.add(list);
+            return;
+        }
+
+        for(int col = 0; col < board[row].length; col++){
+            if(isSafe(board, row, col)){
+                board[row][col] = 'Q';
+                generate(board, row+1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+   private boolean isSafe(char[][] board, int row, int col){
+    
+    // Check column
+    for(int i = 0; i < row; i++){
+        if(board[i][col] == 'Q') return false;
+    }
+
+    // Check left diagonal
+    int maxLeft = Math.min(row, col);
+    for(int i = 1; i <= maxLeft; i++){
+        if(board[row - i][col - i] == 'Q') return false; 
+    }
+
+    // Check right diagonal
+    int maxRight = Math.min(row, board.length - 1 - col);
+    for(int i = 1; i <= maxRight; i++){
+        if(board[row - i][col + i] == 'Q') return false; 
+    }
+
+    return true;
+}
+}
