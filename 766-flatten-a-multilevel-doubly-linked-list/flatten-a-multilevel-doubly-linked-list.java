beb7984e91c8c7b4ebaccd99784cfc0200f8bdda
Gourav Chouhan <gouravchouhan2512@gmail.com>
2026-05-01 07:57:11 +0530

    Time: 1 ms (45.19%) | Memory: 43.6 MB (40.01%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -10,38 +10,28 @@ class Node {
 
 class Solution {
     public Node flatten(Node head) {
-           if (head == null) return head;
-        dfs(head);
-        return head;
-    }
-
-
-    Node dfs(Node node){
-        Node curr = node;
-        Node last = null;
+        if(head == null) return head;
+        Node curr = head;
 
         while(curr != null){
-            Node next = curr.next;
-
             if(curr.child != null){
-                Node childHead = curr.child;
-                Node childTail = dfs(childHead);
+                Node next = curr.next;
+                curr.next = flatten(curr.child);
+                curr.next.prev = curr;
+                curr.child = null;
 
-                curr.next = childHead;
-                childHead.prev = curr;
+                while(curr.next != null)
+                    curr = curr.next;
 
                 if(next != null){
-                      childTail.next = next;
-                    next.prev = childTail;
+                    curr.next = next;
+                    next.prev = curr;
                 }
-
-                curr.child = null;
-                last = childTail;
-                curr = childTail;
-            }else last = curr;
+            }
             curr = curr.next;
         }
 
-        return last;
+        return head;
+
     }
 }
